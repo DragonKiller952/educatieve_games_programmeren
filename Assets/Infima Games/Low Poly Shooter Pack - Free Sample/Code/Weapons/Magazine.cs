b0af8d73d52afe4dbf3d3bff1964c209f6c30972
DragonKiller952 <qjdegroot@gmail.com>
2022-10-24 15:48:57 +0200

    Initial Unity game commit

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Weapons/Magazine.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2021, Infima Games. All Rights Reserved.
+
+using UnityEngine;
+
+namespace InfimaGames.LowPolyShooterPack
+{
+    /// <summary>
+    /// Magazine.
+    /// </summary>
+    public class Magazine : MagazineBehaviour
+    {
+        #region FIELDS SERIALIZED
+
+        [Header("Settings")]
+        
+        [Tooltip("Total Ammunition.")]
+        [SerializeField]
+        private int ammunitionTotal = 50;
+
+        [Tooltip("Total Ammunition.")]
+        [SerializeField]
+        private int clipTotal = 10;
+
+        [Header("Interface")]
+
+        [Tooltip("Interface Sprite.")]
+        [SerializeField]
+        private Sprite sprite;
+
+        #endregion
+
+        #region GETTERS
+
+        /// <summary>
+        /// Ammunition Total.
+        /// </summary>
+        public override int GetAmmunitionTotal() => ammunitionTotal;
+        /// <summary>
+        /// Clip Total.
+        /// </summary>
+        public override int GetClipTotal() => clipTotal;
+        /// <summary>
+        /// Sprite.
+        /// </summary>
+        public override Sprite GetSprite() => sprite;
+
+        #endregion
+    }
+}
---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Weapons/MagazineBehaviour.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2021, Infima Games. All Rights Reserved.
+
+using UnityEngine;
+
+namespace InfimaGames.LowPolyShooterPack
+{
+    /// <summary>
+    /// Magazine Behaviour.
+    /// </summary>
+    public abstract class MagazineBehaviour : MonoBehaviour
+    {
+        #region GETTERS
+        
+        /// <summary>
+        /// Returns The Total Ammunition.
+        /// </summary>
+        public abstract int GetAmmunitionTotal();
+        /// <summary>
+        /// Returns The Total Clip.
+        /// </summary>
+        public abstract int GetClipTotal();
+        /// <summary>
+        /// Returns the Sprite used on the Character's Interface.
+        /// </summary>
+        public abstract Sprite GetSprite();
+
+        #endregion
+    }
+}
